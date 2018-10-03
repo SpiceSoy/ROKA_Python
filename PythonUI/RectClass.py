a1f19b659095e1441498ca0b23d705c62cb5a1fd
unknown <aksghkgh12@gmail.com>
2018-10-03 23:07:20 +0900

    Edit

--- PythonUI/RectClass.py
+++ PythonUI/RectClass.py
@@ -1,11 +1,26 @@
 from tkinter import *
+from Utill import *
+
+
+def swtichColor(state):
+    if state == RectState['block']:
+        print("Draw gray")
+        return 'gray'
+    elif state == RectState['startPoint']:
+        print("Draw lightgreen")
+        return 'lightgreen'
+    elif state == RectState['endPoint']:
+        print("Draw gold")
+        return 'gold'
+    else:
+        return 'white'
 
 class Rect:
-    def __init__(self,canvas,containList,xIndex,yIndex,canvasRectSetting,color='green'):
+    def __init__(self,canvas,containList,xIndex,yIndex,canvasRectSetting,state=RectState['default']):
         self.parentCanvas = canvas
         self.xIndex = xIndex
         self.yIndex = yIndex
-        self.color = color
+        self.state = state
         self.canvasRectSetting = canvasRectSetting
         self.containList = containList
         self.__drawInCanvas()
@@ -16,7 +31,7 @@ class Rect:
             self.canvasRectSetting.marginY + self.yIndex * self.canvasRectSetting.rectSize,
             self.canvasRectSetting.marginX + (self.xIndex * self.canvasRectSetting.rectSize) + self.canvasRectSetting.rectSize,
             self.canvasRectSetting.marginY + (self.yIndex * self.canvasRectSetting.rectSize) + self.canvasRectSetting.rectSize,
-            fill=self.color
+            fill = swtichColor(self.state)
             )
     def __getCenterPointX(self):
             return self.marginX + (self.xIndex * self.rectSize) + (self.rectSize/2)
@@ -24,9 +39,7 @@ class Rect:
             return self.marginY + (self.yIndex * self.rectSize) + (self.rectSize/2)
     def Refresh(self):
         self.__drawInCanvas()
-    def isClicked(self,event,state = 'edit'):
-        #Edit 모드일때는 클릭시 붉은색으로 변화합니다.
-        if state == 'edit':
-            self.color = 'gray'
-            self.Refresh()
+    def isClicked(self,event,state):
+        self.state = state
+        self.Refresh()
         
--- PythonUI/UIStart.py
+++ PythonUI/UIStart.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from RectClass import *
 from canvasUtill import *
+from Utill import *
 
 #메인 윈도우 생성
 mainWindow = Tk()
@@ -15,17 +16,14 @@ drawCanvas.configure(background='black')
 
 #세팅값 설정
 canvasRectSetting = CanvasRectSetting(15,15,30,25)
-class State:
-    def __init__(self,state):
-        self.state = state
-editMode = State(False)
+editMode = EditMode(EditState['default'])
 
 #Rect생성
 rectList = list()
 for yCount in range(canvasRectSetting.rectMax):
     tempYlist = list()
     for xCount in range(canvasRectSetting.rectMax): 
-        tempYlist.append(Rect(drawCanvas,rectList,xCount,yCount,canvasRectSetting,'white'))
+        tempYlist.append(Rect(drawCanvas,rectList,xCount,yCount,canvasRectSetting))
     rectList.append(tempYlist)
 
 
@@ -49,9 +47,9 @@ def canavsClicked(event):
     clickDebugText.configure(
         text = str(event.x) + " , " + str(event.y) + " 위치 클릭 \n " 
         + str(temp[0]) +" , " + str(temp[1]) + "위치의 사각형 클릭" )
-    if editMode.state:
+    if editMode.state != EditState['default']:
          if checkIndex(temp):
-            rectList[temp[1]][temp[0]].isClicked(event)
+            rectList[temp[1]][temp[0]].isClicked(event,editMode.state)
             rectList[temp[1]][temp[0]].Refresh()
 
 drawCanvas.bind("<Button-1>",canavsClicked)
@@ -64,12 +62,18 @@ startButton.pack(fill='x')
 modeButton = Button(toolFrame,text="벽 편집하기")
 modeButton.pack(fill='x')
 def modeChange():
-    if editMode.state == False:
-        modeButton.configure(text = "벽 편집 종료")
-        editMode.state = True
-    else:
+    if editMode.state == EditState['default']:
+        modeButton.configure(text = "시작 지점 편집하기")
+        editMode.state = EditState['editBlock']
+    elif editMode.state == EditState['editBlock']:
+        modeButton.configure(text = "종료지점 편집하기")
+        editMode.state = EditState['editStartPoint']
+    elif editMode.state == EditState['editStartPoint']:
+        modeButton.configure(text = "편집 종료")
+        editMode.state = EditState['editEndPoint']
+    elif editMode.state == EditState['editEndPoint']:
         modeButton.configure(text = "벽 편집하기")
-        editMode.state = False
+        editMode.state = EditState['default']
 modeButton.configure(command=modeChange)
 
 
